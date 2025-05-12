797097ba485d49e235ee1fbbcfab4a5affff294b
johnyb0223 <jonathan.black.work@outlook.com>
2025-05-12 00:31:45 -0400

    iterate

--- updater.py
+++ updater.py
@@ -209,13 +209,13 @@ class Updater(BaseUpdater):
             updater_script = self.configuration.system_path / "update_scripts" / "mac_updater.sh"
             print(f"local download location: {local_download_location}")
             print(f"system path: {self.configuration.system_path}")
-            print(f"executable path: {self.configuration.executable_path}")
+            print(f"executable path: {self.configuration.executable_path.parent.parent}")
             subprocess.Popen(
                 [
                     "bash",
                     str(updater_script),
                     str(local_download_location),
-                    str(self.configuration.executable_path.parent.parent.parent),
+                    str(self.configuration.executable_path.parent.parent),
                 ]
             )
 
