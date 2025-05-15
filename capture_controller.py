cbc0b2aeb7feab7ef52f7f9ffc4e0b96a370d3a1
johnyb0223 <jonathan.black.work@outlook.com>
2025-05-14 20:21:43 -0400

    dependency injection refactor

--- capture_controller.py
+++ capture_controller.py
@@ -1,6 +1,5 @@
 from typing import Optional
-import platform
-from capture_worker import WindowsCaptureWorker, MacCaptureWorker, BaseCaptureWorker
+from capture_worker import BaseCaptureWorker
 from overlay import Overlay
 from PyQt5.QtCore import QThread, Qt, QObject, pyqtSignal
 from logging import Logger
@@ -14,54 +13,56 @@ class CaptureController(QObject):
 
     stopped = pyqtSignal()  # emitted whenever capture ends (graceful or on error)
 
-    def __init__(self, overlay: Overlay, logger: Logger, message_builder: MessageBuilder, text_extractor: TextExtractor, configuration: Configuration, window_identifier: str = "The Bazaar"):
+    def __init__(
+        self,
+        overlay: Overlay,
+        logger: Logger,
+        message_builder: MessageBuilder,
+        text_extractor: TextExtractor,
+        configuration: Configuration,
+        capture_worker: BaseCaptureWorker,
+    ):
         super().__init__()
         self._overlay = overlay
-        self._window_identifier = window_identifier
         self._logger = logger
         self._thread: Optional[QThread] = None
-        self._worker: Optional[BaseCaptureWorker] = None
+        self._capture_worker: BaseCaptureWorker = capture_worker
+        self._current_worker: Optional[BaseCaptureWorker] = None
         self._message_builder: MessageBuilder = message_builder
         self._text_extractor: TextExtractor = text_extractor
         self._configuration = configuration
 
     # ---------------------------- public API ------------------------------
     def running(self) -> bool:
-        return self._worker is not None
+        return self._current_worker is not None
 
     def start(self):
-        if self._worker is not None:
+        if self._current_worker is not None:
             return  # already running
         self._logger.info("Starting CaptureWorker …")
         self._thread = QThread()
-
-        # Create platform-specific worker
-        if platform.system() == "Windows":
-            self._worker = WindowsCaptureWorker(self._window_identifier, self._message_builder, self._text_extractor, self._logger, self._configuration)
-        else:  # macOS
-            self._worker = MacCaptureWorker(self._window_identifier, self._message_builder, self._text_extractor, self._logger, self._configuration)
-
-        self._worker.moveToThread(self._thread)
+        self._current_worker = self._capture_worker
+        self._current_worker.moveToThread(self._thread)
 
         # wire signals
-        self._thread.started.connect(self._worker.start)
-        self._worker.message_ready.connect(self._display_text, Qt.QueuedConnection)
-        self._worker.error.connect(self._on_worker_error)
+        self._thread.started.connect(self._current_worker.start)
+        self._current_worker.message_ready.connect(self._display_text, Qt.QueuedConnection)
+        self._current_worker.error.connect(self._on_worker_error)
 
         self._thread.start()
 
     def stop(self):
-        if self._worker is None or self._thread is None:
+        if self._current_worker is None or self._thread is None:
             return
         self._logger.info("Stopping CaptureWorker …")
         try:
-            self._worker.stop()
+            self._current_worker.stop()
         except Exception:
             self._logger.exception("Error while stopping CaptureWorker")
         self._thread.quit()
         self._thread.wait(3_000)
         self._thread = None
-        self._worker = None
+        self._current_worker = None
         self.stopped.emit()
 
     # ------------------------ internal helpers ----------------------------
---
+++ container.py
@@ -0,0 +1,67 @@
+from logger import logger
+from configuration import Configuration
+from security import Security
+from message_builder import MessageBuilder
+from text_extractor import TextExtractor
+from system_handler import WindowsSystemHandler, MacSystemHandler, BaseSystemHandler
+from updater import TestUpdateSource, ProductionUpdateSource, Updater, BaseUpdateSource
+from overlay import Overlay
+from capture_controller import CaptureController
+from capture_worker import BaseCaptureWorker, CaptureWorkerFactory
+from bazaar_buddy import BazaarBuddy
+from PyQt5.QtWidgets import QApplication
+import sys
+
+
+class Container:
+
+    def __init__(self):
+
+        self.app = QApplication(sys.argv)
+
+        self.logger = logger
+        self.configuration = Configuration()
+
+        self.security = Security(self.configuration, self.logger)
+        self.message_builder = MessageBuilder(self.configuration, self.logger)
+        self.text_extractor = TextExtractor(self.configuration, self.logger)
+
+        self.system_handler: BaseSystemHandler = (
+            WindowsSystemHandler() if self.configuration.operating_system == "Windows" else MacSystemHandler()
+        )
+
+        self.update_source: BaseUpdateSource = (
+            TestUpdateSource(self.logger)
+            if self.configuration.update_with_test_release
+            else ProductionUpdateSource(self.logger)
+        )
+
+        self.overlay: Overlay = Overlay("Checking for updates…", self.configuration)
+
+        self.updater: Updater = Updater(
+            self.overlay, self.logger, self.configuration, self.update_source.latest_release
+        )
+
+        self.controller: CaptureController = CaptureController(
+            self.overlay,
+            self.logger,
+            self.message_builder,
+            self.text_extractor,
+            self.configuration,
+            self.capture_worker,
+        )
+
+        self.bazaar_buddy: BazaarBuddy = BazaarBuddy(
+            self.overlay,
+            self.logger,
+            self.controller,
+            self.system_handler,
+            self.configuration,
+        )
+
+    @property
+    def capture_worker(self) -> BaseCaptureWorker:
+        return CaptureWorkerFactory.create(self.configuration, self.message_builder, self.text_extractor, self.logger)
+
+
+container = Container()
