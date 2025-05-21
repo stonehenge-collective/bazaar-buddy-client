4c242731bdbcf57318d13dd1fda68be0cf1f50c9
dev-jeb <jonathan.black.work@outlook.com>
2025-05-20 21:45:28 -0400

    fix logging messages

--- worker_framework.py
+++ worker_framework.py
@@ -126,7 +126,7 @@ class ThreadController:
         # Store worker and thread
         self.workers[worker.name] = WorkerRecord(worker=worker, thread=thread)
 
-        self._logger.info(f"[[{self._thread_name}]] added worker: {worker.name}")
+        self._logger.info(f"[{self._thread_name}] added worker: {worker.name}")
 
         if auto_start:
             self.start_worker(worker.name)
@@ -150,7 +150,7 @@ class ThreadController:
 
         worker_data = self.workers[worker_name]
         worker_data["thread"].start()
-        self._logger.info(f"[[{self._thread_name}]] started worker: {worker_name}")
+        self._logger.info(f"[{self._thread_name}] started worker: {worker_name}")
         return True
 
     def stop_worker(self, worker_name):
@@ -171,29 +171,29 @@ class ThreadController:
         success = worker_data["thread"].wait(3000)
 
         if success:
-            self._logger.info(f"[[{self._thread_name}]] stopped worker: {worker_name}")
+            self._logger.info(f"[{self._thread_name}] stopped worker: {worker_name}")
             self.workers.pop(worker_name, None)
         else:
-            self._logger.warning(f"[[{self._thread_name}]] worker did not stop cleanly: {worker_name}")
+            self._logger.warning(f"[{self._thread_name}] worker did not stop cleanly: {worker_name}")
 
     def start_all(self):
         """Start all worker threads"""
-        self._logger.info(f"[[{self._thread_name}]] starting all workers")
+        self._logger.info(f"[{self._thread_name}] starting all workers")
         for worker_name in self.workers:
             self.start_worker(worker_name)
 
     def stop_all(self):
         """Stop all worker threads"""
-        self._logger.info(f"[[{self._thread_name}]] stopping all workers")
+        self._logger.info(f"[{self._thread_name}] stopping all workers")
         for worker_name in list(self.workers.keys()):
             self.stop_worker(worker_name)
 
     def cleanup(self):
         """Force cleanup of threads that haven't stopped properly"""
-        self._logger.info(f"[[{self._thread_name}]] performing final cleanup")
+        self._logger.info(f"[{self._thread_name}] performing final cleanup")
         for worker_name, worker_data in list(self.workers.items()):
             if worker_data["thread"].isRunning():
-                self._logger.warning(f"[[{self._thread_name}]] forcing thread to terminate: {worker_name}")
+                self._logger.warning(f"[{self._thread_name}] forcing thread to terminate: {worker_name}")
                 worker_data["thread"].terminate()
                 worker_data["thread"].wait()
             self.workers.pop(worker_name, None)
